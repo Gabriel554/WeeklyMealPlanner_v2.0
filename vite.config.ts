625aae4e48f99eefc4974afaaaf960adaff84c51
Gabriel Gallegos <gabriel.gaby.gallegos@gmail.com>
2025-03-10 17:05:25 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WeeklyMealPlanner_v2.0/',
-  build: {
-    outDir: 'build'
-  }
+  base: "/WeeklyMealPlanner_v2.1",
+  
 })
